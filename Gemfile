eb0d1165b84785fd64f5a51395f37d1ede71dabb
Josh Haywood <josh.haywood1@gmail.com>
2015-05-31 18:37:45 -0400

    show date time picker by adding render to the edit and new .js.erb files

--- Gemfile
+++ Gemfile
@@ -25,10 +25,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'simple_form'
 
-gem 'bootstrap-sass', '~> 3.3.3'
+gem 'bootstrap-sass'
 
-gem 'momentjs-rails', '>= 2.9.0'
-gem 'bootstrap3-datetimepicker-rails', '~> 4.0.0'
+gem 'momentjs-rails'
+gem 'bootstrap3-datetimepicker-rails'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,30 +19,30 @@ class TasksController < ApplicationController
     @task  = Task.create(task_params)
   end
 
- 
-    def update
-     @task.update(task_params) 
+
+  def update
+    @task.update(task_params) 
   end
 
   # DELETE /tasks/1
   # DELETE /tasks/1.json
   def destroy
-     @task.destroy
-    
+    @task.destroy
+
   end
 
   private
 
-     def all_tasks
-      @tasks = Task.all
-    end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
+  def all_tasks
+    @tasks = Task.all
+  end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def task_params
-      params.require(:task).permit(:description, :deadline)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def task_params
+    params.require(:task).permit(:description, :deadline)
+  end
 end
